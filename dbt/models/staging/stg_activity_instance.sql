49b4ac1a0eef039b352dc19e52f1d55295715531
Andrey Kozichev <akozichev@gmail.com>
2024-01-17 16:03:08 +0000

    cast timestamp

--- dbt/models/staging/stg_activity_instance.sql
+++ dbt/models/staging/stg_activity_instance.sql
@@ -1,5 +1,22 @@
 SELECT DISTINCT ON (id)
-    id, taskid, endtime, canceled, starttime, activityid, executionid, removaltime, activityname, activitytype, completescope, durationinmillis, processinstanceid, processdefinitionid, processdefinitionkey, rootprocessinstanceid, calledprocessinstanceid, parentactivityinstanceid 
+    id, 
+    taskid, 
+    endtime::TIMESTAMP, 
+    canceled, 
+    starttime::TIMESTAMP, 
+    activityid, 
+    executionid, 
+    removaltime::TIMESTAMP, 
+    activityname, 
+    activitytype, 
+    completescope, 
+    durationinmillis, 
+    processinstanceid, 
+    processdefinitionid, 
+    processdefinitionkey, 
+    rootprocessinstanceid, 
+    calledprocessinstanceid, 
+    parentactivityinstanceid 
 FROM (  SELECT id, taskid, endtime, canceled, starttime, activityid, executionid, removaltime, activityname, activitytype, completescope, durationinmillis, processinstanceid, processdefinitionid, processdefinitionkey, rootprocessinstanceid, calledprocessinstanceid, parentactivityinstanceid FROM {{ source('camunda_history', 'activity_instance_startedafter') }} 
         UNION  
         SELECT id, taskid, endtime, canceled, starttime, activityid, executionid, removaltime, activityname, activitytype, completescope, durationinmillis, processinstanceid, processdefinitionid, processdefinitionkey, rootprocessinstanceid, calledprocessinstanceid, parentactivityinstanceid FROM {{ source('camunda_history', 'activity_instance_finishedafter') }} 
--- dbt/models/staging/stg_process_instance.sql
+++ dbt/models/staging/stg_process_instance.sql
@@ -1,5 +1,18 @@
 SELECT DISTINCT ON (id)
-    id, state, endtime, starttime, businesskey, removaltime, startactivityid, durationinmillis, processdefinitionid, processdefinitionkey, processdefinitionname, rootprocessinstanceid, superprocessinstanceid, processdefinitionversion 
+    id, 
+    state, 
+    endtime::TIMESTAMP, 
+    starttime::TIMESTAMP, 
+    businesskey, 
+    removaltime::TIMESTAMP, 
+    startactivityid, 
+    durationinmillis, 
+    processdefinitionid, 
+    processdefinitionkey, 
+    processdefinitionname, 
+    rootprocessinstanceid, 
+    superprocessinstanceid, 
+    processdefinitionversion 
 FROM (  SELECT id, state, endtime, starttime, businesskey, removaltime, startactivityid, durationinmillis, processdefinitionid, processdefinitionkey, processdefinitionname, rootprocessinstanceid, superprocessinstanceid, processdefinitionversion FROM {{ source('camunda_history', 'process_instance_startedafter') }} 
         UNION  
         SELECT id, state, endtime, starttime, businesskey, removaltime, startactivityid, durationinmillis, processdefinitionid, processdefinitionkey, processdefinitionname, rootprocessinstanceid, superprocessinstanceid, processdefinitionversion FROM {{ source('camunda_history', 'process_instance_finishedafter') }} 
